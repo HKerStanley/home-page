dca4e2c48a03bca631cfc1a7c421f1eeb4191621
HKerStanley <stanleychiu@protonmail.com>
2022-03-16 22:34:01 +0800

    Update assets

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,20 @@
-import { Image } from "@chakra-ui/react";
+import { Image, useColorMode } from "@chakra-ui/react";
 
 const Footer = () => {
+  const { colorMode } = useColorMode();
   return (
     <a
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      href="https://vercel.com/hkerstanley/home-page"
       target="_blank"
       rel="noopener noreferrer"
     >
       Powered by{" "}
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <Image
+        src={`vercel-logotype-${colorMode}.svg`}
+        alt="Vercel Logo"
+        width={72}
+        height={16}
+      />
     </a>
   );
 };
