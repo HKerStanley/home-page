7ed80ecc0e0fe00e057074c56d53fca1537dedce
HKerStanley <stanleychiu@protonmail.com>
2022-03-16 22:16:28 +0800

    Init clean up

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import { Image } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <a
+      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by{" "}
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    </a>
+  );
+};
+
+export default Footer;
